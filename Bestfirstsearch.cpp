eedc534d1756a051bf06e09b9b60a7ae43da422e
Swaraj Phand <swarajphand@gmail.com>
2024-10-23 13:33:26 +0530

    commit

--- Bestfirstsearch.cpp
+++ Bestfirstsearch.cpp
@@ -1,73 +1,83 @@
-// C++ program to implement Best First Search using priority
-// queue
-#include <bits/stdc++.h>
+// C++ program to implement Best-First Search using priority queue
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-typedef pair<int, int> pi;
 
-vector<vector<pi> > graph;
+// Alias for a pair of integers (cost, node)
+using NodeCostPair = pair<int, int>;
 
-// Function for adding edges to graph
-void addedge(int x, int y, int cost)
-{
-	graph[x].push_back(make_pair(cost, y));
-	graph[y].push_back(make_pair(cost, x));
+// Graph represented as an adjacency list
+vector<vector<NodeCostPair>> adjList;
+
+// Function to add bidirectional edges with weights
+void addEdge(int src, int dest, int weight) {
+    adjList[src].push_back({weight, dest});
+    adjList[dest].push_back({weight, src});
 }
 
-// Function For Implementing Best First Search
-// Gives output path having lowest cost
-void best_first_search(int actual_Src, int target, int n)
-{
-	vector<bool> visited(n, false);
-	// MIN HEAP priority queue
-	priority_queue<pi, vector<pi>, greater<pi> > pq;
-	// sorting in pq gets done by first value of pair
-	pq.push(make_pair(0, actual_Src));
-	int s = actual_Src;
-	visited[s] = true;
-	while (!pq.empty()) {
-		int x = pq.top().second;
-		// Displaying the path having lowest cost
-		cout << x << " ";
-		pq.pop();
-		if (x == target)
-			break;
+// Function to perform Best-First Search from startNode to endNode
+void bestFirstSearch(int startNode, int endNode, int totalNodes) {
+    // Vector to keep track of visited nodes
+    vector<bool> isVisited(totalNodes, false);
 
-		for (int i = 0; i < graph[x].size(); i++) {
-			if (!visited[graph[x][i].second]) {
-				visited[graph[x][i].second] = true;
-				pq.push(make_pair(graph[x][i].first,graph[x][i].second));
-			}
-		}
-	}
+    // Priority queue for exploring nodes with minimum cost first (min-heap)
+    priority_queue<NodeCostPair, vector<NodeCostPair>, greater<NodeCostPair>> minHeap;
+    minHeap.push({0, startNode});
+    isVisited[startNode] = true;
+
+    // Perform search until all nodes are explored or target is reached
+    while (!minHeap.empty()) {
+        int currentNode = minHeap.top().second;
+        cout << currentNode << " ";  // Output the path
+        minHeap.pop();
+
+        // If the target node is reached, exit the loop
+        if (currentNode == endNode) {
+            break;
+        }
+
+        // Explore all neighboring nodes of the current node
+        for (const auto& neighbor : adjList[currentNode]) {
+            int neighborNode = neighbor.second;
+            int pathCost = neighbor.first;
+
+            // Only add unvisited neighbors to the priority queue
+            if (!isVisited[neighborNode]) {
+                isVisited[neighborNode] = true;
+                minHeap.push({pathCost, neighborNode});
+            }
+        }
+    }
+    cout << endl;
 }
 
-// Driver code to test above methods
-int main()
-{
-	// No. of Nodes
-	int v = 14;
-	
-	
-	graph.resize(v);
-    addedge(0, 1, 3);
-	addedge(0, 2, 6);
-	addedge(0, 3, 5);
-	addedge(1, 4, 9);
-	addedge(1, 5, 8);
-	addedge(2, 6, 12);
-	addedge(2, 7, 14);
-	addedge(3, 8, 7);
-	addedge(8, 9, 5);
-	addedge(8, 10, 6);
-	addedge(9, 11, 1);
-	addedge(9, 12, 10);
-	addedge(9, 13, 2);
+// Main function to test Best-First Search
+int main() {
+    // Number of nodes in the graph
+    int nodeCount = 14;
+    adjList.resize(nodeCount);
 
-	int source = 0;
-	int target = 9;
+    // Adding edges with their respective weights
+    addEdge(0, 1, 3);
+    addEdge(0, 2, 6);
+    addEdge(0, 3, 5);
+    addEdge(1, 4, 9);
+    addEdge(1, 5, 8);
+    addEdge(2, 6, 12);
+    addEdge(2, 7, 14);
+    addEdge(3, 8, 7);
+    addEdge(8, 9, 5);
+    addEdge(8, 10, 6);
+    addEdge(9, 11, 1);
+    addEdge(9, 12, 10);
+    addEdge(9, 13, 2);
 
-	// Function call
-	best_first_search(source, target, v);
+    int start = 0;  // Starting node
+    int goal = 9;   // Target node
 
-	return 0;
+    // Call the Best-First Search function
+    bestFirstSearch(start, goal, nodeCount);
+
+    return 0;
 }
